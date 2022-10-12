145513d21b8d4e06a2ca84ad52e938cba7a1206d
tunapro1234 <tunagul54@gmail.com>
2022-10-12 23:56:38 +0300

    mpu6050 data class deleted

--- src/main/java/frc/robot/lib/mpu6050/MPU6050.java
+++ src/main/java/frc/robot/lib/mpu6050/MPU6050.java
@@ -1,7 +1,5 @@
 package frc.robot.lib.mpu6050;
 
-//import frc.robot.lib.mpu6050.MPUData;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.I2C;
 import java.nio.ByteBuffer;
@@ -15,6 +13,27 @@ public class MPU6050 {
   private ByteBuffer m_readBuffer = ByteBuffer.allocate(14);
   private byte m_I2CAddress;
   private I2C m_i2c;
+
+  private static class MPUData {
+    public short m_accX, m_accY, m_accZ, m_gyroX, m_gyroY, m_gyroZ;
+    private short m_temperature;
+
+    public MPUData(ByteBuffer MPUReading) {
+      m_accX = MPUReading.getShort(0);
+      m_accY = MPUReading.getShort(2);
+      m_accZ = MPUReading.getShort(4);
+      m_temperature = MPUReading.getShort(6);
+      m_gyroX = MPUReading.getShort(8);
+      m_gyroY = MPUReading.getShort(10);
+      m_gyroZ = MPUReading.getShort(12);
+    }
+
+    public double getTemperature() {
+      // Sıcaklığı hesaplamak için biraz işlem
+      return m_temperature/340. + 36.53;
+    }
+
+  }
 
   // 0x68
   public MPU6050(byte I2CAddress) {
@@ -34,7 +53,7 @@ public class MPU6050 {
     return m_I2CAddress;
   }
 
-  public MPUData getMPUData() {
+  private MPUData getMPUData() {
     m_i2c.read(0x3B, 14, m_readBuffer);
     return new MPUData(m_readBuffer);
   }
